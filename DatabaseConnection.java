a931465c0d62ee58a4a62b25366b671c9678aa13
shivaa4445 <shivaa4445@gmail.com>
2024-11-09 22:36:49 +0530

    Create DatabaseConnection.java

---
+++ DatabaseConnection.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/library_management";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "yourpassword"; // replace with your MySQL password
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
