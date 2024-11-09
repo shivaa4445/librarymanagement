c5f5baa940c617df4fd1e7330f73ed95d3395b56
shivaa4445 <shivaa4445@gmail.com>
2024-11-09 22:39:00 +0530

    Create UserServlet.java

---
+++ UserServlet.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.sql.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/UserServlet")
+public class UserServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    // Register user
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        try {
+            Connection con = DBConnection.getConnection();
+            String query = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, name);
+            ps.setString(2, email);
+            ps.setString(3, password);
+            ps.executeUpdate();
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        response.sendRedirect("login.html");
+    }
+}
